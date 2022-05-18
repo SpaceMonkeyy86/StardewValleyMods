0420da5bb47e17dc6c3d0395fa7c5a106a6e7d27
TwinBuilderOne <jackreynolds9128@gmail.com>
2022-05-18 17:58:46 -0500

    Add project files.

---
+++ MoreBundles/BundleDataLoader.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using StardewModdingAPI;
+using System.IO;
+
+namespace MoreBundles
+{
+    class BundleDataLoader : IAssetLoader
+    {
+        public bool CanLoad<T>(IAssetInfo asset) => asset.AssetNameEquals("Data\\ExpertBundles");
+        public T Load<T>(IAssetInfo asset) => JsonConvert.DeserializeObject<T>(File.ReadAllText(Path.Combine(ModEntry.ModHelper.DirectoryPath, "assets", "Bundles.json")));
+    }
+}
